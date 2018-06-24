f7a31c00580072e0b681b2073a5dd26abd9c7444
Arnold C. Sinko <arnoldcsinko@gmail.com>
2018-06-24 17:57:03 -0400

    Add redux-thunk

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,56 +1,48 @@
 // Import Dependencies
-import { 
-  React,
-  Component,
-  connect,
-  CardList,
-  SearchBox,
-  Header,
-  changeSearchField,
-  getUsers,
-} from './imports';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import CardList from '../../components/CardList';
+import SearchBox from '../../components/SearchBox';
+import Header from '../../components/Header';
+import { changeSearchField, requestRobots } from '../../actions';
+
 
 import './App.css';
 import 'tachyons';
 
-const mapStateToProps = state => ({
-  search: state.search  
+const mapStateToProps = ({ searchRobots, requestRobots }) => ({
+  search: searchRobots.search,
+  robots: requestRobots.robots,
+  ispending: requestRobots.ispending,
+  error: requestRobots.error
 });
 
 const mapDispatchToProps = dispatch => ({
-  onSearchChange: (event) => dispatch(changeSearchField(event.target.value))
+  onSearchChange: (event) => dispatch(changeSearchField(event.target.value)),
+  onRequestRobots: () => dispatch(requestRobots())
 });
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      robots: [],
-      
-    }
+  componentDidMount() {
+    this.props.onRequestRobots();
   }
 
-  componentDidMount() {    
-    getUsers()
-      .then(users => this.setState({ robots: users }))
-  }  
-
   render() {
-    const { robots } = this.state;
-    const {search, onSearchChange } = this.props;
+    const { search, onSearchChange, robots, isPending } = this.props;
     const filteredRobots =
       robots.filter(robot =>
         robot.name.toLowerCase()
-          .includes(search.toLowerCase()))
-    return (
-      <div className="App">
-        <Header>
-          <h1 className="app-title">Meet The Monsters</h1>
-          <SearchBox handleChange={onSearchChange} />
-        </Header>
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+          .includes(search.toLowerCase()));
+    return isPending ? <h1>Loading...</h1> :
+      (
+        <div className="App">
+          <Header>
+            <h1 className="app-title">Meet The Monsters</h1>
+            <SearchBox handleChange={onSearchChange} />
+          </Header>
+          <CardList robots={filteredRobots} />
+        </div>
+      );
   }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 // Import Dependencies
-import {
-    React,
-    ReactDOM,
-    Provider,
-    createStore,
-    searchRobots,
-    App,
-    registerServiceWorker,
-} from './imports';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { searchRobots, requestRobots } from './reducers';
+import App from './containers/App/App';
+import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
-const store = createStore(searchRobots);
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const logger = createLogger();
+const store = createStore(
+        rootReducer,
+        applyMiddleware(thunkMiddleware, logger)
+);
 ReactDOM.render(
     <Provider store={store}>
         <App/>
